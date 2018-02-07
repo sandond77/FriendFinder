a2c774c533250f114781d5163edcbb1a52376297
sandond77 <sandu@ucdavis.edu>
2018-02-07 00:16:30 -0800

    minor bug fixes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 
+var app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
