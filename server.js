54c2dd86376a0982cd3e0f6305280796316bd27e
sandond77 <sandu@ucdavis.edu>
2018-02-07 00:14:05 -0800

    finished routing

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
