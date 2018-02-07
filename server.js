d8e8dd8c056e2994d800d04414c517c99bd7b036
sandond77 <sandu@ucdavis.edu>
2018-02-07 01:31:54 -0800

    submit and api routing working

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("routing/apiRoutes.js")(app);
-require("routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
