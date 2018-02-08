57d4aaa8ec4c2ffe417c4ff508c6beed9d0d6e95
sandond77 <sandu@ucdavis.edu>
2018-02-07 22:22:11 -0800

    matching logic 90% complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,25 @@ module.exports = function(app) {
   	});
 
 	app.post("/api/friends", function(req, res) {
-      friendsData.push(req.body);
-      res.json(true);
+      	friendsData.push(req.body);
+      	res.json(true);
+
+      	var totalDifference;
+      	var matchScore;
+      	var matchName;
+    	for (var i = 0; i < friendsData.length; i++) {
+    		totalDifference = 0;
+    		matchScore = -1;
+    		for (var j = 0; j < friendsData[i].scores.length; j++) {
+    			totalDifference += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(req.body.scores[j]));
+    		}
+
+    		if (matchScore < totalDifference && req.body.name !== friendsData[i].name) {
+    			matchScore = totalDifference;
+    			matchName = friendsData[i].name;
+    		}
+    	}
+
+    	console.log("match", matchName)
   });
 }
