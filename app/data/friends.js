201018910257fcd65d2087c2d3563e867d950c9f
sandond77 <sandu@ucdavis.edu>
2018-06-26 01:08:54 -0700

    added more people to API

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
-        "name": "Might Guy",
-        "photo": "https://vignette.wikia.nocookie.net/naruto/images/3/31/Might_Guy.png/revision/latest?cb=20150401084456",
+        "name": "Bob Ross",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Bob_at_Easel.jpg/220px-Bob_at_Easel.jpg",
         "scores": [
             "1",
             "1",
@@ -13,6 +13,70 @@ var friendsArray = [
             "1",
             "1",
             "1"
+        ]
+    },
+    {
+        "name": "Anthony Bourdain",
+        "photo": "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2018/06/gettyimages-620646350-h_2018.jpg",
+        "scores": [
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "5",
+            "1",
+            "1",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Mr.Rogers",
+        "photo": "https://daily.jstor.org/wp-content/uploads/2018/03/mister_rogers_feature_2_1050x700.jpg",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Obama",
+        "photo": "https://static.politico.com/8c/6b/373e8cf14c4cbb3526193ef54f08/180622-obama.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Bill Clinton",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Bill_Clinton_%2830349165325%29_%28cropped%29.jpg/220px-Bill_Clinton_%2830349165325%29_%28cropped%29.jpg",
+        "scores": [
+            "1",
+            "1",
+            "2",
+            "2",
+            "3",
+            "3",
+            "4",
+            "4",
+            "5",
+            "5"
         ]
     }
 ]
